b5589ed639d77923087f2c12ae3fbfd37b61a970
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-01-17 22:18:05 +0800

    chore: format

--- grammar.js
+++ grammar.js
@@ -1,26 +1,10 @@
 module.exports = grammar({
   name: "qmldir",
   rules: {
-    source_file: ($) => seq(
-      repeat(seq(
-        $.line,
-        "\n",
-      )),
-      optional($.line)
-    ),
-    line: ($) => repeat1(
-      choice(
-        $.number,
-        $.varname,
-        $.spliter,
-      )
-    ),
+    source_file: ($) => seq(repeat(seq($.line, "\n")), optional($.line)),
+    line: ($) => repeat1(choice($.number, $.varname, $.spliter)),
     number: (_) => /[\d]+/,
     varname: (_) => /[A-Za-z\-_]+/,
-    spliter: (_) => choice(
-      '.',
-      '/',
-      ':'
-    ),
+    spliter: (_) => choice(".", "/", ":"),
   },
 });
